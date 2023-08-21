138bc392653f7779cd4a8d9e9b40d40a204ae1e9
Davit Sedrakyan <davosedrakya17n@gmail.com>
2023-08-21 22:40:15 +0400

    add change sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git DEVELOP");
         System.out.println("Learning!");
-        System.out.println("It is terribly difficult");
+        System.out.println("It is terribly difficult fgfg");
         System.out.println("I am happy");
         System.out.println("!!!!");
     }
