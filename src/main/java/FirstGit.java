305f57fccacefb6025f3d6eae090594d73653fd5
Davit Sedrakyan <davosedrakya17n@gmail.com>
2023-08-21 22:39:32 +0400

    del sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,8 +1,7 @@
 public class FirstGit {
     public static void main(String[] args) {
-        System.out.println("Git DEVELOP");
+        System.out.println("Git");
         System.out.println("Learning!");
-        System.out.println("It is terribly difficult");
         System.out.println("I am happy");
         System.out.println("!!!!");
     }
