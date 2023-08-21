134c3e4b4e36cd58b21ffeb7059e5ef5fd0e061c
Greyhaund17 <130869352+Greyhaund17@users.noreply.github.com>
2023-08-21 21:42:05 +0400

    Update FirstGit.java
    Add sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git");
         System.out.println("Learning!");
-
+        System.out.println("I am happy");
     }
 
 }
